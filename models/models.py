1e36e96c29865673da357227238faa794ec9c07d
Moataman <motaman1996@hotmail.com>
2018-11-26 13:05:47 +0200

    Custmer & Meal models init

--- models/models.py
+++ models/models.py
@@ -2,6 +2,33 @@
 
 from odoo import models, fields, api
 
+class Customer(models.Model):
+	_name = 'feed.customer'
+	"""docstring for Customer"""
+    _inherit = ['res.partner']
+
+    customer_previous_orders = fields.One2many('feed.order','customer_id')
+
+
+class Meal(models.Model):
+
+	_name = feed.meal
+	_inherit = ['lunch.product']
+
+	meal_category = fields.Char(related= lunch.product.category)
+
+
+	"""docstring for Meal"models.Modelf 
+
+	_na__init__(self, arg):
+		super(Meal,models.Model._
+
+		_na_init__()
+		self.arg = arg
+		
+
+		
+
 # class feed_me(models.Model):
 #     _name = 'feed_me.feed_me'
 
