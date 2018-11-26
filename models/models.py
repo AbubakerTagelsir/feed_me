88bb20a0d180d5c6eff17f862c8a5e70e010655e
amlkamal <tamnosa7@gmail.com>
2018-11-26 06:06:14 -0500

    init order model

--- models/models.py
+++ models/models.py
@@ -3,13 +3,46 @@
 from odoo import models, fields, api
 
 # class feed_me(models.Model):
-#     _name = 'feed_me.feed_me'
+class FeedOrder(models.Model):
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    _inherit = ['sale.order']
+    
+    customer_id = fields.Many2one('feed.customer', "Reference")
+    resturant_id = fields.Many2one('feed.resturant', "Reference")
+    
+    order_location=fields.text(string='Location',)
+
+    # selected_meal_items
+    resturantMeal_id = fields.Many2one('feed.resturant.meal')
+    meal_price = fields.float(related="feed.resturant.meal.price")
+    meal_perparation_time=fields.float(related="feed.resturant.meal.meal_perpartion_time")
+    meal_quantity=fields.Integer("Quantity")
+   
+
+    total_order_price = fields.Float("Total Order Price", compute="_get_the_order_price")
+
+
+    @api.multi
+    @api.onchange('resturantMeal_id')
+    def _get_total_order_price(self): 
+        print(self)   
+        total = 0.00
+        for i in self:
+            for meal in i.resturantMeal_id:
+                total += meal.meal_price * meal.meal_quantity
+            i.total_order_price = total
+
+
+    total_order_time = fields.Float("Total Order time", compute="_get_the_order_time")
+
+    @api.multi
+    @api.onchange('resturantMeal_id')
+    def _get_total_order_time(self): 
+        print(self)   
+        total_time = 0.00
+        for i in self:
+            for meal in i.resturantMeal_id:
+                total_time += meal.meal_perparation_time * meal.meal_quantity
+            i.total_order_time = total_time
+
+
