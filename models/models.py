af093c7236c2721915d25d264efa1b07c43d5949
Baraah <baraahadil369@outlook.com>
2018-11-27 12:35:32 +0200

    New Branch added

--- models/models.py
+++ models/models.py
@@ -5,8 +5,7 @@ from odoo import models, fields, api
 class Customer(models.Model):
 	_name = 'feed.customer'
 	_inherit = ['res.partner']
-	customer_previous_orders = fields.One2many('feed.order','customer_id')
-	print("----------------------customer --------------------")
+	customer_previous_orders = fields.One2many('sale.order','customer_id')
 
 class Meal(models.Model):
 	_name = 'feed.meal'
@@ -18,27 +17,25 @@ class Meal(models.Model):
 
 # class feed_me(models.Model):
 class Order(models.Model):
-	_name = 'feed.order'
-	_inherit = ['sale.order']
+	_inherit = 'sale.order'
+	restaurant_id = fields.Many2one('res.company', "Restaurant")
 	customer_id = fields.Many2one('feed.customer', "Reference")
-	resturant_id = fields.Many2one('feed.resturant', "Reference")
-	res_meal_id = fields.Many2one('feed.resturant.meal', "Reference")
 	order_location=fields.Text(string='Location',)
 
 	# selected_meal_items
-	resturantMeal_id = fields.Many2one('feed.resturant.meal')
-	meal_perparation_time=fields.Float(related="res_meal_id.meal_preparation_time")
+	resturant_meal_id = fields.Many2one('feed.restaurant.meal')
+	meal_preparation_time=fields.Float(related="resturant_meal_id.meal_preparation_time")
 	meal_quantity=fields.Integer("Quantity")
-
+	delivery = fields.Many2one('feed.delivery')
 
 
 	@api.multi
-	@api.onchange('resturantMeal_id')
+	@api.onchange('resturant_meal_id')
 	def _get_total_order_price(self): 
 	    print(self)   
 	    total = 0.00
 	    for i in self:
-	        for meal in i.resturantMeal_id:
+	        for meal in i.resturant_meal_id:
 	            total += meal.price * meal.meal_quantity
 	        i.total_order_price = total
 
@@ -46,61 +43,62 @@ class Order(models.Model):
 	total_order_time = fields.Float("Total Order time", compute="_get_the_order_time")
 
 	@api.multi
-	@api.onchange('resturantMeal_id')
+	@api.onchange('resturant_meal_id')
 	def _get_total_order_time(self): 
 	    print(self)   
 	    total_time = 0.00
 	    for i in self:
-	        for meal in i.resturantMeal_id:
-	            total_time += meal.meal_perparation_time * meal.meal_quantity
+	        for meal in i.resturant_meal_id:
+	            total_time += meal.meal_preparation_time * meal.meal_quantity
 	        i.total_order_time = total_time
 
 
-class Resturant(models.Model):
-	_name = 'feed.resturant'
+class Restaurant(models.Model):
+	_inherit = 'res.company'
 	description = fields.Text()
 	name = fields.Char()
+	restaurant_pic = fields.Binary("Image" , attachment=True)
 	location = fields.Text()
-	menu = fields.One2many('feed.resturant.meal', 'resturant_id','Menu')
-	order_id = fields.One2many('feed.order', 'resturant_id','Order')
-	order_preparation_time = fields.Float(compute="get_preparation_time")
+	menu = fields.One2many('feed.restaurant.meal', 'restaurant_id','Menu')
+	current_orders = fields.One2many('sale.order', 'restaurant_id','Order')
+	#odoo hr schedule
 	active_from = fields.Datetime()
 	active_to = fields.Datetime()
 	active = fields.Boolean(compute="is_active_now")
-	#add categories of food??
+	sections = fields.One2many('feed.restaurant.section','restaurant_id')
 
-	class Delivery(models.Model):
-		_name = 'feed.delivery'
-		delivery_man_name = fields.Char()
-		delivery_man_phone = fields.Integer()
-		delivery_man_location = fields.Text()
-		delivery_man_status = fields.Selection(
-	        string='Status',
-	        selection=[('active', 'Active'), ('unactive', 'Unactive')],
-		    default = 'Unactive')
-		order_id = fields.Many2one('feed.order')
-		delivery_man_history= fields.Char(related="order_id.name")
-			
+	def is_active_now(self):
+		time = fields.datetime.now()
+		self.active = time >= self.active_from and time <= self.active_to
+
+class RestaurantSection(models.Model):
+	_name = 'feed.restaurant.section'
+	name = fields.Char()
+	restaurant_id = fields.Many2one('res.company')
+	#capacity
+	
+
+class Delivery(models.Model):
+	_name = 'feed.delivery'
+	delivery_man_name = fields.Char()
+	delivery_man_phone = fields.Integer()
+	delivery_man_location = fields.Text()
+	delivery_man_status = fields.Selection(
+        string='Status',
+        selection=[('active', 'Active'), ('unactive', 'Unactive')],
+	    default = 'Unactive')
+	current_orders = fields.One2many('sale.order', 'delivery')
 		
-		def is_active_now(self):
-			time = fields.datetime.now()
-			self.active = time >= self.active_from and time <= self.active_to
+	
+	
 
 
-		def get_preparation_time(self):
-			total = 0
-			for order in self:
-				total+= order.ResturantMeal_id.meal_preparation_time
-
-			self.order_preparation_time = total
-
-
-class ResturantMeal(models.Model):
-	_name = 'feed.resturant.meal'
+class RestaurantMeal(models.Model):
+	_name = 'feed.restaurant.meal'
 	_inherit = 'feed.meal'
 	description = fields.Text()
 	name = fields.Char()
-	resturant_id = fields.Many2one('feed.resturant')
+	restaurant_id = fields.Many2one('res.company')
 	meal_preparation_time = fields.Float()
 	price = fields.Float()
 	#add categories of food??
